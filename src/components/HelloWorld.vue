265160ea3f6e29658d675a845ad0fcdf3f70fb6d
Witt <jiwt@inspur.com>
2020-06-12 21:01:09 +0800

    init projectr

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,82 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h3>{{ title }}</h3>
+    
+    <dl>
+      <dt>{{title}}</dt>
+      <dd><button @click="changeTitle()">ChangeTitle</button></dd>
+    </dl>
+    
+    <dl>
+      <dt>Employees</dt>
+      <dd>
+        <ul>
+          <li v-for="employee in employees" :key="employee.id">
+            {{employee.name}} &nbsp; <button @click="removeEmployee(employee.id)">删除</button>
+          </li>
+        </ul>
+      </dd>
+      <dd>共【{{employeesCount}}】个员工</dd>
+      <dd><button @click="addEmployee()">新增</button>&nbsp;</dd>
+    </dl>
+
+    <dl>
+      <dt>Test</dt>
+      <dd>{{test}}</dd>
+      <dd>
+        <button @click="changeTest()">ChangeTest</button>
+      </dd>
+    </dl>
   </div>
 </template>
 
 <script>
+
+window.test = 'This is a test.';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  computed: {
+    title: function() {
+      return this.$store.getters.title;
+    },
+    
+    employees: function() {
+      return this.$store.state.employees;
+    },
+    
+    employeesCount: function() {
+      return this.$store.getters.employeesCount;
+    },
+    
+    test: function() {
+      return window.test;
+    }
+  },
+  
+  methods: {
+    
+    changeTitle: function() {
+      const random = Math.random();
+      this.$store.commit('changeTitle', random);
+    },
+    
+    addEmployee: function() {
+      this.$store.dispatch('addEmployee');
+    },
+    
+    removeEmployee: function(id) {
+      this.$store.dispatch('removeEmployee', id);
+    },
+    
+    
+    changeTest: function() {
+      window.test = `This is a test ...【${(new Date()).toISOString()}】`;
+      console.log(window.test);
+      console.log(this.test);
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,52 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    title: 'Learn Vue Vuex',
+    employees: [
+      {id: 1, code: 'Emp-0001', name: '员工-0001'},
+      {id: 2, code: 'Emp-0002', name: '员工-0002'}
+    ]
+  },
+  getters: {
+    title: (state) => {
+      return `【${state.title}】`;
+    },
+    employeesCount: (state) => {
+      return state.employees.length;
+    }
   },
   mutations: {
+    changeTitle: (state, payload)=> {
+      const now = (new Date()).toISOString();
+      state.title = `【Title】: ${now}-${payload}`;
+    },
+
+    addEmployee: (state, newEmployee) => {
+      state.employees.push(newEmployee);
+    },
+    
+    removeEmployee: (state, idToRemove) => {
+      const indexToRemove = state.employees.findIndex((employee) => {
+        return employee.id === idToRemove;
+      });
+      state.employees.splice(indexToRemove, 1);
+    }
   },
   actions: {
+
+    addEmployee(context) {
+      const random = Math.round(Math.random() * 10000);
+      const newEmployee = {
+        id: random,
+        code: `Emp-${random}`,
+        name: `员工-${random}`
+      };
+      context.commit('addEmployee', newEmployee);
+    },
+
+    removeEmployee(context, idToRemove) {
+      context.commit('removeEmployee', idToRemove);
+    }
   },
   modules: {
   }
